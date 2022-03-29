dc9861b6065011da0e523f4f4d8c3de8049a8893
Paul Oh <paul.oh1221@gmail.com>
2022-03-29 18:20:08 -0400

    Generate proof (#1)
    * initial post req function for verifying proof
    
    * logged each steps in the proof call
    
    * createInput function that creates the entire input json from leaf
    
    * es lint fixes
    
    * removed unnecessary files
    
    * proof api accepts all the inputs
    
    * declared vkey globally and fixed eslint syntax

--- src/controllers/proof.ts
+++ src/controllers/proof.ts
@@ -1,10 +1,33 @@
+import * as fs from 'fs'
+import * as snarkjs from 'snarkjs'
 import { Body, Controller, Post } from 'amala'
 import ProofBody from '@/validators/ProofBody'
+
+const vKey = JSON.parse(
+  fs.readFileSync('./pot/verification_key.json').toString()
+)
 
 @Controller('/proof')
 export default class ProofController {
   @Post('/')
-  proof(@Body({ required: true }) { leaf }: ProofBody) {
-    return { leaf }
+  async proof(@Body({ required: true }) input: ProofBody) {
+    console.log('Generating witness and creating proof!')
+
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+      input,
+      './build/OwnershipChecker_js/OwnershipChecker.wasm',
+      './pot/OwnershipChecker_final.zkey'
+    )
+
+    console.log('Verifying proof!')
+    const res = await snarkjs.groth16.verify(vKey, publicSignals, proof)
+
+    if (res) {
+      console.log('Verified!')
+    } else {
+      console.log('Invalid proof')
+    }
+
+    return proof
   }
 }
---
+++ src/types/msgInput.d.ts
@@ -0,0 +1,6 @@
+export declare type MsgInput = {
+  r: string[]
+  s: string[]
+  msghash: string[]
+  pubkey: string[][]
+}
