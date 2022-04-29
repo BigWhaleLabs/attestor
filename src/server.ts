105a3ab70dfeb30d06c31d6d6d90ab2ad52ed0fa
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-29 08:39:50 -0700

    add cluster

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'module-alias/register'
 import 'source-map-support/register'
 
 import cleanJobs from '@/helpers/cleanJobs'
+import cluster from 'cluster'
 import runApp from '@/helpers/runApp'
 import runMongo from '@/helpers/mongo'
 import startJobChecker from '@/helpers/jobs'
@@ -13,7 +14,18 @@ void (async () => {
   await cleanJobs()
   console.log('Starting app...')
   await runApp()
-  console.log('Starting job checker...')
-  startJobChecker()
   console.log('Launch sequence completed 🚀')
 })()
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`)
+  cluster.fork()
+
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  console.log(`Worker ${process.pid} started, starting the job checker...`)
+  startJobChecker()
+  console.log('Started job checker')
+}
