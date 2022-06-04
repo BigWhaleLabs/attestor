39ec749395e5ca680d923f895755fdb8c623090f
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-04 09:32:12 -0700

    fix imap

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -33,46 +33,49 @@ async function check() {
     return
   }
   checking = true
-  await client.connect()
-  let lock: MailboxLockObject | undefined
   try {
     console.log('Checking for new messages...')
-    lock = await client.getMailboxLock('INBOX')
-    const result = client.fetch('1:*', {
-      envelope: true,
-      source: false,
-      bodyStructure: true,
-      uid: true,
-    })
-    const messages = [] as FetchMessageObject[]
-    for await (const message of result) {
-      const address = message.envelope.from[0].address
-      if (!address) {
-        return
-      }
-      const hexMessage = Buffer.from(address, 'utf8').toString('hex')
-      const signature = await ed.sign(hexMessage, env.EDDSA_PRIVATE_KEY)
-      const signatureHexString = Buffer.from(signature).toString('hex')
-      console.log(`Replying to ${address}, ${signatureHexString}`)
-      await transporter.sendMail({
-        from: `"SealCred" <${user}>`,
-        to: address,
-        subject: "Here's your token!",
-        text: `Your token is: ${signatureHexString}`,
+    const lock = await client.getMailboxLock('INBOX')
+    try {
+      const result = client.fetch('1:*', {
+        envelope: true,
+        source: false,
+        bodyStructure: true,
+        uid: true,
       })
-      messages.push(message)
+      const messages = [] as FetchMessageObject[]
+      for await (const message of result) {
+        const address = message.envelope.from[0].address
+        if (!address) {
+          return
+        }
+        const hexMessage = Buffer.from(address, 'utf8').toString('hex')
+        const signature = await ed.sign(hexMessage, env.EDDSA_PRIVATE_KEY)
+        const signatureHexString = Buffer.from(signature).toString('hex')
+        console.log(`Replying to ${address}, ${signatureHexString}`)
+        await transporter.sendMail({
+          from: `"SealCred" <${user}>`,
+          to: address,
+          subject: "Here's your token!",
+          text: `Your token is: ${signatureHexString}`,
+        })
+        messages.push(message)
+      }
+      await Promise.all(
+        messages.map((message) =>
+          client.messageDelete(`${message.uid}`, { uid: true })
+        )
+      )
+    } finally {
+      lock.release()
     }
-    await Promise.all(
-      messages.map((message) => client.messageDelete(`${message.uid}`))
-    )
   } finally {
-    lock?.release()
-    await client.logout()
     checking = false
   }
 }
 
 export default async function setupMailer() {
+  await client.connect()
   await check()
   setInterval(check, 1000 * 5)
 }
