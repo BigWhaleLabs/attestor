dda1fbbbb9cd6262b78406c95fe944d8ab48fc50
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-10 07:52:34 -0700

    add nullifier

--- src/helpers/eddsaSigFromString.ts
+++ src/helpers/eddsaSigFromString.ts
@@ -1,10 +1,13 @@
 import { buildEddsa, buildMimc7 } from 'circomlibjs'
 import { utils } from 'ethers'
+import cryptoRandomString from 'crypto-random-string'
 import env from '@/helpers/env'
 
 export default async function (message: string) {
   // Message
-  const messageUInt8 = utils.toUtf8Bytes(message)
+  const messageUInt8 = utils.toUtf8Bytes(
+    `${message}-${cryptoRandomString({ length: 6 })}`
+  )
   const mimc7 = await buildMimc7()
   const M = mimc7.multiHash(messageUInt8)
   // EdDSA
