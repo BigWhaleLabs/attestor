a5e740b166890d6d5c1d3dbd0d710effb8a0285a
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-19 10:53:09 -0700

    add clustering

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
 import 'module-alias/register'
 import 'source-map-support/register'
 
+import * as os from 'os'
+import Cluster from '@/helpers/cluster'
 import runApp from '@/helpers/runApp'
 
-void (async () => {
-  console.log('Starting app...')
-  await runApp()
-  console.log('Server launch sequence completed 🚀')
-})()
+const totalCPUs = os.cpus().length
+
+if (Cluster.isPrimary) {
+  console.log(`Number of CPUs is ${totalCPUs}`)
+  console.log(`Primary ${process.pid} is running`)
+
+  // Fork workers.
+  for (let i = 0; i < totalCPUs; i++) {
+    Cluster.fork()
+  }
+
+  Cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`)
+    console.log("Let's fork another worker!")
+    Cluster.fork()
+  })
+} else {
+  void runApp()
+  console.log(`Worker ${process.pid} started`)
+}
