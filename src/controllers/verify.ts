39533e9baa3e9390f2f82f5cd8d3e89bfdbf9109
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-09-07 08:00:35 -0700

    add sorting

--- src/controllers/verify.ts
+++ src/controllers/verify.ts
@@ -68,7 +68,7 @@ export default class VerifyController {
 
   @Post('/email')
   async sendEmail(@Body({ required: true }) { emails }: EmailVerifyBody) {
-    for (const email of emails) {
+    for (const email of emails.sort()) {
       const domain = email.split('@')[1].toLowerCase()
       const domainBytes = padZeroesOnRightUint8(utils.toUtf8Bytes(domain), 90)
       const signature = await eddsaSigFromString(domainBytes)
