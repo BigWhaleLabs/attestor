30b0b9bffb189aee5613bc7ca26a39c6d8e98241
Michel Erokhov <mixaileroxov@gmail.com>
2022-09-12 19:42:28 +0300

    check if url is undefined

--- src/controllers/verify.ts
+++ src/controllers/verify.ts
@@ -131,17 +131,20 @@ export default class VerifyController {
       rinkebyProvider
     )
     const formatedUsername = formatBytes32String(username)
-    const url = (await contract.usernameToUrl(formatedUsername)).replace(
-      'directory',
-      'proof'
-    )
+    const url = await contract.usernameToUrl(formatedUsername)
+    if (!url) {
+      return ctx.throw(badRequest(`Can't find user`))
+    }
+    const infoUrl = url.replace('directory', 'proof')
     const {
       data: { signerAddress },
     } = await axios.get<{
       signerAddress: string
-    }>(url)
+    }>(infoUrl)
     if (signerAddress?.toLowerCase() !== address.toLowerCase()) {
-      return ctx.throw(badRequest("Connected address doesn't match"))
+      return ctx.throw(
+        badRequest(`Couldn't find a user with this username ${username}`)
+      )
     }
     // Generate EDDSA signature
     const eddsaMessage = `${address.toLowerCase()}ownsfarcaster`
