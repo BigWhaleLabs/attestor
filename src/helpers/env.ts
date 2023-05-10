89f8828df6891b181942678561b834321aaf31aa
Michel Erokhov <mixaileroxov@gmail.com>
2023-05-10 19:23:13 +0200

    fix sorting

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv'
 import {
+  ETH_MUMBAI_NETWORK,
   ETH_NETWORK,
   ETH_RPC,
   ETH_RPC_MAINNET,
-  ETH_MUMBAI_NETWORK,
 } from '@big-whale-labs/constants'
 import { cleanEnv, num, str } from 'envalid'
 import { cwd } from 'process'
@@ -17,9 +17,9 @@ export default cleanEnv(process.env, {
   EDDSA_PRIVATE_KEY: str(),
   ETH_NETWORK: str({ default: ETH_NETWORK }),
   ETH_POLYGON_NETWORK: str({ default: ETH_MUMBAI_NETWORK }),
-  ETH_RPC_POLYGON: str(),
   ETH_RPC: str({ default: ETH_RPC }),
   ETH_RPC_MAINNET: str({ default: ETH_RPC_MAINNET }),
+  ETH_RPC_POLYGON: str(),
   PORT: num({ default: 1337 }),
   SMTP_PASS: str(),
   SMTP_USER: str(),
