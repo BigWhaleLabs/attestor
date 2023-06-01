9acf3e0733fc1bd516e6049f38db3a88813966cc
Michel Erokhov <mixaileroxov@gmail.com>
2023-06-01 15:29:04 +0200

    fix token verification

--- src/controllers/verify-token.ts
+++ src/controllers/verify-token.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Post } from 'amala'
 import TokenBody from '@/validators/TokenBody'
-import signAttestationMessage from '@/helpers/signatures/signAttestationMessage'
 import hexlifyString from '@/helpers/hexlifyString'
+import signAttestationMessage from '@/helpers/signatures/signAttestationMessage'
 
 @Controller('/verify-token')
 export default class VerifyTokenController {
   @Post('/token')
   token(@Body({ required: true }) { token, type }: TokenBody) {
-    return signAttestationMessage(type, hexlifyString(token)))
+    return signAttestationMessage(type, hexlifyString(token))
   }
 }
