adc27a20690a864899882561be3b7e6c1f1f6c36
Michel Erokhov <mixaileroxov@gmail.com>
2023-11-01 16:25:31 +0100

    disable cache for checkInvite

--- src/helpers/ketl/checkInvite.ts
+++ src/helpers/ketl/checkInvite.ts
@@ -1,12 +1,13 @@
-import axios from 'axios'
+import axiosWithCache from 'helpers/axiosWithCache'
 import env from '@/helpers/env'
 
 export default async function checkInvite(
   attestationType: number,
   hash: string
 ) {
-  const { data } = await axios.get<boolean>(
-    `${env.KETL_INVITES_BACKEND}/merkle/hash?attestationType=${attestationType}&hash=${hash}`
+  const { data } = await axiosWithCache.get<boolean>(
+    `${env.KETL_INVITES_BACKEND}/merkle/hash?attestationType=${attestationType}&hash=${hash}`,
+    { cache: false }
   )
 
   console.log(
